3a307b39a569f4e5749deb07ac428ed611664b35
Developmentexpert121 <developmentexpert121@gmail.com>
2025-10-08 17:46:34 +0530

    updated code

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-26df!pps&2(^rbg*5&tb&pt5dtwutva*d66umeiw@+ez79t-=8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
